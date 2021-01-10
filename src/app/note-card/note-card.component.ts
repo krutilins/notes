21a64f9d07ca8174230dee49b1253c6b639a9483
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2021-01-10 16:54:04 +0300

    Add list of note card

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,36 @@
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements OnInit, AfterViewInit {
+
+  @Input() title: string;
+  @Input() body: string;
+
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    // work out if there is a text overflow and if not, then hide the truncator
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      // if there is a text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      // else (there is a text overflow), hide the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+
+}
